9161109c0e291671e08177156b7ab0234c46da64
Błażej Domagała <petitoff@outlook.com>
2022-06-12 23:36:49 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+
+// start session
+session_start();
+
+require_once("php/CreateDb.php");
+require_once('./php/component.php');
+
+// create instance of CreateDb class
+$database = new CreateDb("Productdb", "Producttb");
+
+if (isset($_POST['add'])) {
+    /// print_r($_POST['product_id']);
+    if (isset($_SESSION['cart'])) {
+
+        $item_array_id = array_column($_SESSION['cart'], "product_id");
+
+        if (in_array($_POST['product_id'], $item_array_id)) {
+            echo "<script>alert('Produkt został już dodany do koszyka!')</script>";
+            echo "<script>window.location = 'index.php'</script>";
+        } else {
+
+            $count = count($_SESSION['cart']);
+            $item_array = array(
+                'product_id' => $_POST['product_id']
+            );
+
+            $_SESSION['cart'][$count] = $item_array;
+        }
+
+    } else {
+
+        $item_array = array(
+            'product_id' => $_POST['product_id']
+        );
+
+        // Create new session variable
+        $_SESSION['cart'][0] = $item_array;
+        print_r($_SESSION['cart']);
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>eShop</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css" />
+
+    <!-- Font Awesome -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+      integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+
+    <!-- Bootstrap CDN -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+
+    <?php
+        require_once('php/header.php');
+    ?>
+    <div class="container">
+      <div class="row text-center py-5">
+          <?php
+              $result = $database->getData();
+              while ($row = mysqli_fetch_assoc($result)){
+                  component($row['product_name'], $row['product_price'], $row['product_image'], $row['id']);
+              }
+          ?>
+      </div>
+    </div>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
